fcb786a695ae03799c504300ab2d6f8491176c6c
Sekar, Anantharaman <Anantharaman.Sekar@philips.com>
2024-09-03 12:01:38 +0530

    fixed issue

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,7 @@
 #include "stats.h"
 #include <numeric>
+#include <algorithm>
+//using namespace std;
 
 
 using namespace Statistics;
@@ -8,9 +10,9 @@ Stats Statistics::ComputeStatistics(const std::vector<double>& elements) {
     //Implement statistics here
     Stats result;
 
-    result.average = accumulate(elements.begin(), elements.end(), 0.0 / elements.size());
+    result.average = accumulate(elements.begin(), elements.end(), 0.0 )/ elements.size();
         result.min = *(std::min(elements.begin(), elements.end()));
-        result.max = *(std::max(elements.begin(), elements.end()));
+        result.max = *(std::max_element(elements.begin(), elements.end()));
 
     return result; 
 }
